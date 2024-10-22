655cd2e7c4d00ade3a47e4bc967403bbd074afcd
PraneethRiser <117887153+Praneeth1628@users.noreply.github.com>
2024-10-22 10:57:32 +0530

    Update Devops_team20.java

--- Devops_Team20/Devops/Devops_team20.java
+++ Devops_Team20/Devops/Devops_team20.java
@@ -4,7 +4,7 @@ public class Devops_team20 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Running Devops.....!");
+		System.out.println("Welcome Devops Lab.....!");
 	}
 
 }
