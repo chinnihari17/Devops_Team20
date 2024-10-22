2bfa993897dbf611f7a2b19e14820f46129973fa
91898 <91898@LAPTOP-QOQDH6F2>
2024-10-22 10:45:50 +0530

    Commit

---
+++ Devops_Team20/Devops/Devops_team20.java
@@ -0,0 +1,10 @@
+package Devops;
+
+public class Devops_team20 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Running Devops.....!");
+	}
+
+}
